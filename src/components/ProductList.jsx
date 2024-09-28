3871f6d88ea8c63ed3f93d085378690eb1a0c74c
Ajay Dangi <ajaydangi233@gmail.com>
2024-09-28 22:16:50 +0530

    c

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../api/productsAPI';
 import { setProducts, incrementSkip } from '../redux/actions';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 const ProductList = () => {
-  const { products,loading, category, searchQuery, skip } = useSelector(state => state);
+  const { products, loading, category, searchQuery, skip } = useSelector(state => state);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -22,18 +24,24 @@ const ProductList = () => {
 
 
   return (
-    <div className='text-xl mb-5 mt-10'>
-      {products.map(product => (
-        <div key={product.id} className='mb-6 rounded-2xl shadow-lg'>
-           <img 
-              src={product.thumbnail} 
-              alt={product.title} 
-              className="object-cover rounded-md " 
-            />
-          <h4 className='text-xl font-sans font-bold '>Product name: {product.title}</h4>
-          <p className='text-xl font-sans '>Description: {product.description}</p>
-        </div>
-      ))}
+    <div className='container mx-auto  text-xl mb-5 mt-10 '>
+      <div className='grid grid-cols-3 gap-4'>
+        {products.map(product => (
+          <div>
+            <Card key={product.id} style={{ width: '18rem' }} className='container mx-auto shadow-lg '>
+              <Card.Img variant="top" src={product.thumbnail} alt={product.title} />
+              <Card.Body>
+                <Card.Title className='font-bold'> {product.title}</Card.Title>
+                <Card.Text>
+                  {product.description}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
+
       {loading && <p>Loading...</p>}
       <button className=' h-10 w-40 text-center text-white mr-5 mt-3 shadow-lg rounded-2xl bg-blue-400' onClick={loadMore} disabled={loading}>Load More</button>
       <button >Total Product: {products.length}</button>
